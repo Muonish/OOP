6fde045077290f7833504d0f36c73e7384851100
Muonish <breshko314@gmail.com>
2014-10-01 00:54:38 +0300

    lab1-3+begin_4

---
+++ OOPpaint1/LibraryTrapezium/LibraryTrapezium/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LibraryTrapezium
+{
+    public interface IApplicationFunc
+    {
+        void Go();
+    }
+
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class)]
+    public class InfoAttribute : System.Attribute
+    {
+        public string CompanyName { get; set; }
+        public string CompanyUrl { get; set; }
+    }
+}
